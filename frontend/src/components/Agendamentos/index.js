54632c3a05e6622f7af418dd42a6c3a0c3ba484e
Jhonatan Nunes <Jhonatan.souzanunes@hotmail.com>
2021-07-08 13:42:18 -0300

    Get na api agendamentos pronto

--- frontend/src/components/Agendamentos/index.js
+++ frontend/src/components/Agendamentos/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -9,30 +9,17 @@ import TableRow from '@material-ui/core/TableRow';
 import Titulo from '../Titulo';
 import axios from 'axios';
 
-async function getUser() {
+async function getAgendamentos() {
   try {
-    const response = await axios.get('/user?ID=12345');
+    const response = await axios.get('http://localhost:4000/api/agendamentopg');
     console.log(response);
+    return response.data.agenda
   } catch (error) {
     console.error(error);
   }
 }
 
 
-
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
-];
 
 function preventDefault(event) {
   event.preventDefault();
@@ -46,27 +33,42 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Agendamentos() {
   const classes = useStyles();
+
+  const [agendamento, setAgendamentos] = useState([]);
+
+  useEffect(async () => {
+    try {
+      let a = await getAgendamentos();
+      setAgendamentos(a);
+    } catch (error) {
+      console.log(error)
+    }
+
+  }, []);
+  console.log(agendamento)
+
+
   return (
     <React.Fragment>
       <Titulo>Agendamentos</Titulo>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>CPF</TableCell>
+            <TableCell>Nome</TableCell>
+            <TableCell>Observação</TableCell>
+            <TableCell>Nescess. Especiais</TableCell>
+            <TableCell align="right">Data/Hora</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {agendamento?.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell>{row.cpf_pessoa}</TableCell>
+              <TableCell>{row.nome_pessoa}</TableCell>
+              <TableCell>{row.obs_agendamento}</TableCell>
+              <TableCell>{row.necess_especiais}</TableCell>
+              <TableCell align="right">{row.data_hora_agendamento}</TableCell>
             </TableRow>
           ))}
         </TableBody>
